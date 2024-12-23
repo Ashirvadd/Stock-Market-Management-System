8314efef79692f25b031befff9dd62ac478e306e
Aviral Singh <aviralpratapsingh2002@gmail.com>
2024-12-23 23:05:08 +0530

    Initial commit with login and signup functionality

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './Components/LandingPage/LandingPage';
+import Login from './Components/Login/Login';
+import Signup from './Components/Signup/signup';
+import Home from './Pages/Home';
+import './App.css';
+// import { Home } from '@mui/icons-material';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;

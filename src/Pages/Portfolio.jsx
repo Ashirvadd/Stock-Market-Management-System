60a441840e45c6c6db1b1832c3718a18808d8d1c
Aviral Singh <aviralpratapsingh2002@gmail.com>
2025-01-16 19:36:31 +0530

    Changed navbar and some styling

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { SparklesCore } from "D:/Web Development/DBMS/stock-visualizer/dbms-project/src/Components/ui/sparkles.jsx";
+//import { SparklesCore } from "D:/Web Development/DBMS/stock-visualizer/dbms-project/src/Components/ui/sparkles.jsx";
 import Navbar from "../Components/Navbar/Navbar";
-
+import { SparklesCore } from "../Components/ui/sparkles";
 const Trade = () => {
   const [add, setAdd] = useState("");
   const [withdraw, setWithdraw] = useState("");
@@ -29,7 +29,7 @@ const Trade = () => {
   return (
     <div>
       <Navbar />
-      <div className="text-center text-white mb-4">
+      <div className="text-center text-white mt-[68px] mb-4">
         <p className="text-lg">Current Balance: ${balance}</p>
       </div>
     </div>
